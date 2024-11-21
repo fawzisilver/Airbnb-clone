d8256b3f25ede2baeef603a8810c7fc4e805dbbb
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-11-20 20:08:27 -0700

    useDebounce library (adds delay), added logic to navSearch (search now works yay)

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,9 +1,43 @@
-import { Input } from "../ui/input"
+"use client";
+import { Input } from "../ui/input";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { useState, useEffect } from "react";
 
 const NavSearch = () => {
-  return (
-    <Input type="text" placeholder="look for property..." className="max-w-lg dark:bg-muted" />
-)
-}
+	const searchParams = useSearchParams();
+	const pathName = usePathname();
+	const { replace } = useRouter();
 
-export default NavSearch
+	const [search, setSearch] = useState(searchParams.get("search")?.toString() || "");
+
+	const handleSearch = useDebouncedCallback((value: string) => {
+		const params = new URLSearchParams(searchParams);
+		if (value) {
+			params.set("search", value);
+		} else {
+			params.delete("search");
+		}
+		replace(`${pathName}?${params.toString()}`);
+	}, 500);
+
+	useEffect(() => {
+		if (!searchParams.get("search")) {
+			setSearch("");
+		}
+	}, [searchParams.get("search")]);
+	return (
+		<Input
+			type="text"
+			placeholder="look for property..."
+			className="max-w-lg dark:bg-muted"
+			onChange={(e) => {
+				setSearch(e.target.value);
+				handleSearch(e.target.value);
+			}}
+			value={search}
+		/>
+	);
+};
+
+export default NavSearch;
