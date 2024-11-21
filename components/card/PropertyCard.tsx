26c142fe2a170a48cf4bfa78cae362b5704a0f67
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-11-20 18:11:14 -0700

    added temporary property rating (hardcoded atm)

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -1,13 +1,44 @@
 import Link from "next/link";
+import Image from "next/image";
 import CountryFlagAndName from "./CountryFlagAndName";
 import PropertyRating from "./PropertyRating";
 import FavoriteToggleButton from "./FavoriteToggleButton";
 import { PropertyCardProps } from "@/app/utils/types";
-
+import { formatCurrency } from "@/app/utils/format";
 function PropertyCard({ property }: { property: PropertyCardProps }) {
-	const { name, image, price } = property;
+	const { name, image, price, country, id: propertyId, tagline } = property;
 
-	return <div>PropertyCard</div>;
+	return (
+		<article className="group relative">
+			<Link href={`/properties/${propertyId}`}>
+				<div className="relative h-[300px] mb-2 overflow-hidden rounded-md">
+					<Image
+						src={image}
+						fill
+						// sizes="(max-width:768px) 100vw, 50vw"
+						alt={name}
+						className="rounded-md object-cover transform group-hover:scale-110 transition-transform duration-500"
+					/>
+				</div>
+				<div className="flex justify-between items-center">
+					<h3 className="text-sm font-semibold mt-1">{name.substring(0, 30)}</h3>
+					{/** property rating */}
+					<PropertyRating inPage={false} propertyId={propertyId} />
+				</div>
+				<p className="text-sm mt-1 text-muted-foreground">
+					{tagline.substring(0, 40)}
+				</p>
+				<div className="flex justify-between items-center mt-1">
+					<p className="text-sm mt-1">
+						<span className="font-semibold">{formatCurrency(price)}</span>
+						/night
+					</p>
+					{/** country and flag */}
+				</div>
+			</Link>
+			<div className="absolute top-5 right z-5">{/** favorite toggle button */}</div>
+		</article>
+	);
 }
 
 export default PropertyCard;
