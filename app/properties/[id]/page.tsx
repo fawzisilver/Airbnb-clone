0871baf6960db06ce7718b1aac65038962ce26b4
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-11-25 20:25:19 -0700

    submit review button added

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -14,6 +14,7 @@ import Amenities from "@/components/properties/Amenities";
 // import DynamicMap from "@/app/utils/DynamicMap";
 import dynamic from "next/dynamic";
 import { Skeleton } from "@/components/ui/skeleton";
+import SubmitReview from "@/components/reviews/SubmitReview";
 
 const DynamicMap = dynamic(() => import("@/components/properties/PropertyMap"), {
 	ssr: false,
@@ -60,6 +61,7 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
 					<BookingCalendar />
 				</div>
 			</section>
+			<SubmitReview propertyId={property.id} />
 		</section>
 	);
 }
--- components/reviews/SubmitReview.tsx
+++ components/reviews/SubmitReview.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
 
-function SubmitReview() {
-	return <div>SubmitReview</div>;
+import { useState } from "react";
+import { SubmitButton } from "../form/Button";
+import FormContainer from "../form/FormContainer";
+import { Card } from "../ui/card";
+import RatingInput from "../form/RatingInput";
+import TextAreaInput from "../form/TextAreaInput";
+import { Button } from "../ui/button";
+import { createReviewAction } from "@/app/utils/actions";
+
+function SubmitReview({ propertyId }: { propertyId: string }) {
+	const [isReviewFormVisible, setIsReviewFormVisible] = useState(false);
+
+	return (
+		<div className="mt-8 ">
+			<Button onClick={() => setIsReviewFormVisible((prev) => !prev)}>
+				Leave a Review
+			</Button>
+			{isReviewFormVisible && (
+				<Card className="p-8 mt-8">
+					<FormContainer action={createReviewAction}>
+						<input type="hidden" name="propertyId" value={propertyId} />
+						<RatingInput name="rating" />{" "}
+						{/**needs to match the prisma rating name */}
+						<TextAreaInput
+							name="comment"
+							labelText="your thoughts on this property"
+							defaultValue="Amazing place!"
+						/>
+						<SubmitButton text="Submit review" className="mt-4" />
+					</FormContainer>
+				</Card>
+			)}
+		</div>
+	);
 }
 
 export default SubmitReview;
