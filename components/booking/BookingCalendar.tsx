21d393986ff6b703e9e50cd9524d62afe4b722af
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-12-01 15:03:05 -0700

    added Booking table along with its actions (logic) for adding and deleting booking

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -15,19 +15,41 @@ import {
 function BookingCalendar() {
 	const currentDate = new Date();
 	const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+	const bookings = useProperty((state) => state.bookings);
+	const { toast } = useToast();
 
+	const blockedPeriods = generateBlockedPeriods({ bookings, today: currentDate });
+
+	const unavailableDates = generateDisabledDates(blockedPeriods);
+	console.log(unavailableDates);
 	//if we choose range (setRange) then update state
 	useEffect(() => {
+		//check if date is book
+		const selectedRange = generateDateRange(range);
+		const isDisabledDateIncluded = selectedRange.some((date) => {
+			if (unavailableDates[date]) {
+				setRange(defaultSelected);
+				toast({
+					description: "Some dates are booked. Please select again	",
+				});
+				return true;
+			}
+			return false;
+		});
+
+		// set date range of selected
 		useProperty.setState({ range });
 	}, [range]);
 
 	return (
+		// generates Calendar
 		<Calendar
 			mode="range"
 			defaultMonth={currentDate}
 			selected={range}
 			onSelect={setRange}
 			className="mb-4"
+			disabled={blockedPeriods}
 		/>
 	);
 }
